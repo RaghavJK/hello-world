64df2bc16b28fa633795eeaab56d1cc76d1fa15d
Raghavendra JK <M1047926@host.docker.internal>
2020-03-22 17:01:04 +0530

    added date to hello world program

--- src/main/java/com/jenkins/demo/helloworld/HelloWorld.java
+++ src/main/java/com/jenkins/demo/helloworld/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.jenkins.demo.helloworld;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HelloWorld {
 
-  public static void main(final String[] args) {
+	public static void main(final String[] args) {
+		String pattern = "dd-MM-yyyy";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		String today = simpleDateFormat.format(new Date());
 
-    System.out.println("Hello, World!");
-  }
+		System.out.println("Hello, World! Today is : " + today);
+	}
 
 }
